23e64fa86610a7a9edb23ff2e5e0a793f4cc7903
Kai Andersson <34894196+KaiAndersson@users.noreply.github.com>
2025-12-04 17:02:17 -0500

    removed framerate dependency of turn speed

--- Assets/Cart.cs
+++ Assets/Cart.cs
@@ -4,9 +4,9 @@ using Unity.Mathematics;
 
 public class Cart : MonoBehaviour
 {
-	private float _humanAccel;
+	private float _humanMoveInput;
 	private float _accelSmooth;
-	private float _humanTurn;
+	private float _humanTurnInput;
 	private float _turnSmooth;
 	[SerializeField]
 	private float _accelSmoothingSpeed;
@@ -25,38 +25,39 @@ public class Cart : MonoBehaviour
 	}
 
 	/// <summary>
-	/// Per physics frame tick.
+	/// Physics tick updates cart transform.
 	/// </summary>
 	private void Update()
 	{
 		// Capture human input.
-		_humanAccel = Mathf.Clamp(Input.GetAxisRaw("Vertical"), -1f, 1f);
-		_humanTurn = Mathf.Clamp(-Input.GetAxisRaw("Horizontal"), -1f, 1f) * Mathf.Sqrt(Mathf.Abs(_accelSmooth)) * Mathf.Sign(_accelSmooth);
+		_humanMoveInput = Mathf.Clamp(Input.GetAxisRaw("Vertical"), -1f, 1f);
+		_humanTurnInput = Mathf.Clamp(-Input.GetAxisRaw("Horizontal"), -1f, 1f) * Mathf.Sqrt(Mathf.Abs(_accelSmooth)) * Mathf.Sign(_accelSmooth);
+		// ^^^ (Must scale by forward motion to prevent turning in place.)
 		
-		// Smooth input.
-		_turnSmooth = Mathf.MoveTowards(_turnSmooth, _humanTurn, _turnSmoothingSpeed * Time.deltaTime);
-		_accelSmooth = Mathf.MoveTowards(_accelSmooth, _humanAccel, _accelSmoothingSpeed * Time.deltaTime);
+		// Perform smoothing on input to make motion less jarring (may eliminate if AI doesn't do well with learning to handle the additional complexity).
+		_turnSmooth = Mathf.MoveTowards(_turnSmooth, _humanTurnInput, _turnSmoothingSpeed * Time.deltaTime);
+		_accelSmooth = Mathf.MoveTowards(_accelSmooth, _humanMoveInput, _accelSmoothingSpeed * Time.deltaTime);
 		
 		// Apply acceleration.
-		transform.eulerAngles += _turnSmooth * _turnSpeed * Vector3.forward;
+		transform.eulerAngles += _turnSmooth * _turnSpeed * Vector3.forward * Time.deltaTime;
 		float3 cartPos = transform.position;
 		float3 forward = transform.up;
 		cartPos += _accelSmooth * _speed * forward * Time.deltaTime;
 		
-		// Track edge collisions.
+		// Here we handle 'collisions' with the edge of the track.
+		// Cart position in local space of spline:
 		Vector3 localCartPos = _splineExtrude.transform.InverseTransformPoint(cartPos);
+		// Nearest point along spline to cart pos, in spline's local space:
 		SplineUtility.GetNearestPoint(_splineExtrude.Spline, localCartPos, out float3 nearest, out float t, 4, 2);
+		// Nearest point along spline to cart pos, in world space:
 		nearest = _splineExtrude.transform.TransformPoint(nearest);
-		print("Nearest point: " + nearest);
 		float3 toSpline = nearest - cartPos;
-		print("To Spline: " + toSpline);
 		float distance = math.length(toSpline);
-		print("Distance to nearest point: " + distance);
 		float3 direction = math.normalize(toSpline + math.float3(0.0001f));
 		float splineRadius = _splineExtrude.Radius;
 		float depenetration = Mathf.Max(0f, distance - splineRadius);
 		float3 finalPos = cartPos + depenetration * direction;
-		
+		// Clamp transform onto track:
 		transform.position = finalPos;
 	}
 }
